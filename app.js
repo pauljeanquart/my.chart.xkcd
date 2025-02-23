4e576f22f4de78552eab5474cae13d715c6a5124
Paul Jeanquart <pauljeanquart@gmail.com>
2025-02-22 21:33:26 -0600

    refactor to pas config as data element

---
+++ app.js
@@ -0,0 +1,1 @@
+import './js-xkcd.js';
--- bundle.mjs
+++ bundle.mjs
@@ -3,7 +3,9 @@ import * as esbuild from 'esbuild'
 console.log('esbuild');
 
 await esbuild.build({
-  entryPoints: ['index.js'],
+  entryPoints: ['app.js'],
   bundle: true,
+  minify: true,
+  sourcemap: true,
   outfile: 'out.js',
 })
