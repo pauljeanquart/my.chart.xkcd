23212ddb588f833944984c71b6d30c048e49fada
Paul Jeanquart <pauljeanquart@gmail.com>
2025-02-23 21:31:02 -0600

    simplify pre-copy element

--- js-showCode.js
+++ js-showCode.js
@@ -2,14 +2,16 @@ export function initializeShowCode() {
   const selectedElements = document.querySelectorAll('.js-show-code');
   for (var i = 0, selectedElement; selectedElement = selectedElements[i]; i++) {
     showCodeFor = document.getElementById(selectedElement.getAttribute('for'));
-    selectedElement.innerHTML = replaceAngleBrackets(showCodeFor.outerHTML);
+    selectedElement.innerHTML += replaceAngleBrackets(showCodeFor.outerHTML);
     selectedElement.addEventListener("click", (event) => {
       navigator.clipboard.writeText(event.target.textContent);
-      event.target.parentNode.querySelector(".js-copied").classList.remove("d-none");
-      setTimeout(function () {
-        event.target.parentNode.querySelector(".js-copied").classList.add("d-none");
-        document.getSelection().removeAllRanges();
-      }, 1000);
+      if (event.target.querySelector(".js-show-code-copied")) {
+        event.target.querySelector(".js-show-code-copied").classList.remove("d-none");
+        setTimeout(function () {
+          event.target.querySelector(".js-show-code-copied").classList.add("d-none");
+          document.getSelection().removeAllRanges();
+        }, 1000);
+      }
     });
   }
 }
